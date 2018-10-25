ac0e59f81a3fdfbb19de68b4921f3057ae9a5214
Mohamed4Taha <44267537+Mohamed4Taha@users.noreply.github.com>
2018-10-25 02:18:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,3 +31,34 @@ client.user.setGame(`FIVE SERVER`,"http://twitch.tv/S-F")
 client.login(process.env.BOT_TOKEN);
 
 
+//الرد التلقائي
+
+client.on('message', msg => {
+    if (msg.content === 'هلا') {
+       msg.reply('هلا بيك ');
+      }
+    });
+
+    client.on('message', msg => {
+        if (msg.content === 'اهلا') {
+           msg.reply('و سهلا');
+          }
+        });
+
+        client.on('message', msg => {
+            if (msg.content === 'ممكن كريديت بليز') {
+               msg.reply('مش هينفع اسف ... بس قريبا انشاء الله ممكن نعمل مسابقات و ممكن نعمل نظام دعوات');
+              }
+            });
+
+client.on('message', msg => {
+    if (msg.content === 'اوك') {
+       msg.reply('اوك');
+      }
+    });
+
+client.on('message', msg => {
+    if (msg.content === 'K') {
+       msg.reply('ok');
+      }
+    });

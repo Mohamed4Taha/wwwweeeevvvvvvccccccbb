360fb78b4279b76a9bded5601d0f73761e3448e6
Mohamed4Taha <44267537+Mohamed4Taha@users.noreply.github.com>
2018-10-26 12:33:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -622,3 +622,24 @@ if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return mess
   }
  
 });
+
+
+
+//كود النشر
+
+const Discord = require('discord.js');
+const A7MD = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم 
+لو سمحت ممكن تدخل سيرفرى
+                               [ https://discord.gg/jnuwC6H  ] 
+الدعووة خاصة لك ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})

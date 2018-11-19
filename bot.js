ad07ddb3a209b705499a610fed1b39b8332b2c11
Mohamed4Taha <44267537+Mohamed4Taha@users.noreply.github.com>
2018-11-20 01:13:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -918,3 +918,23 @@ client.on('message', message => {
         message.channel.sendEmbed(embed);
     }
 });
+
+
+
+
+
+//روم الاقتراحات
+
+client.on('message', function(message) {
+    if (message.channel.type === "dm") {
+        if (message.author.id === client.user.id) return;
+        var stewart = new Discord.RichEmbed()
+            .setColor('PURPLE')
+            .setTimestamp()
+            .setTitle('``رساله جديده في خاص البوت``')
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
+            .setFooter(`من (@${message.author.tag})  |  (${message.author.id})`)
+        client.channels.get("504787147631624202").send({ embed: stewart });
+    }
+});

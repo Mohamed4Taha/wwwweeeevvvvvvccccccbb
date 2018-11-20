2e45ad8fbda445cd152d7eecabb476765c8b3fbd
Mohamed4Taha <44267537+Mohamed4Taha@users.noreply.github.com>
2018-11-20 20:43:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -109,21 +109,6 @@ client.on('guildMemberAdd', member => {
       channel.sendEmbed(embed);
     });
 	
-    client.on('guildMemberRemove', member => {
-        var embed = new Discord.RichEmbed()
-        .setAuthor(member.user.username, member.user.avatarURL)
-        .setThumbnail(member.user.avatarURL)
-        .setTitle(`بس بعرف وين رحت؟؟؟ :raised_hand::skin-tone-1: :pensive:`)
-        .setDescription(`مع السلامه تشرفنا بك :raised_hand::skin-tone-1: :pensive: `)
-        .addField('👤   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
-        .setColor('PURPLE')
-        .setFooter(`====اهلا السيرفر نور بيك و الله====`, 'https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
-    
-    var channel =member.guild.channels.find('name', '♛『five』♛')
-    if (!channel) return;
-    channel.send({embed : embed});
-
-    });
     
 	
 	
